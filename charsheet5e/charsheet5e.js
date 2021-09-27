21bd34288ff7e7b3c372da5ee1a6b936cdc19bfb
davidrogers090 <42214669+davidrogers090@users.noreply.github.com>
2021-09-27 10:03:57 -0700

    Add files via upload

---
+++ charsheet5e/charsheet5e.js
@@ -0,0 +1,24 @@
+import { DND5E } from "../../systems/dnd5e/module/config.js";
+import ActorSheet5e from "../../systems/dnd5e/module/actor/sheets/base.js";
+import ActorSheet5eCharacter from "../../systems/dnd5e/module/actor/sheets/character.js";
+
+export class Knifey5eSheet extends ActorSheet5eCharacter {
+  get template() {
+    return "modules/charsheet5e/templates/character-sheet.html";
+  }
+}
+
+Actors.registerSheet("dnd5e", Knifey5eSheet, {
+  types: ["character"],
+  makeDefault: false,
+	label: "Knifey's Alternate 5e Sheet"
+});
+
+Hooks.once("init", () => {
+	const templatePaths = [
+		// Actor Sheet Partials
+		"modules/alt5e/templates/parts/actor-spellbook.html",
+	];
+	// Load the template parts
+	return loadTemplates(templatePaths);
+});
---
+++ charsheet5e/module.js
@@ -0,0 +1,29 @@
+{
+  "name": "charsheet5e",
+  "title": "Knifey's Alternate 5th Edition Dungeons & Dragons Sheet",
+  "description": "Replaces the default D&D 5e character sheet with an alternate layout.",
+  "author": "Knifey",
+  "authors": [],
+  "url": "",
+  "flags": {},
+  "version": "1.6.0",
+  "minimumCoreVersion": "0.8.9",
+  "compatibleCoreVersion": "0.8.9",
+  "scripts": [],
+  "esmodules": [
+    "charsheet5e.js"
+  ],
+  "styles": [],
+  "languages": [],
+  "packs": [],
+  "system": [
+    "dnd5e"
+  ],
+  "dependencies": [],
+  "socket": false,
+  "manifest": "https://raw.githubusercontent.com/davidrogers090/foundry-charsheet/main/module.json",
+  "download": "https://github.com/davidrogers090/",
+  "protected": false,
+  "coreTranslation": false,
+  "minimumSystemVersion": "1.6.0"
+}
